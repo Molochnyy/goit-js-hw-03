3cee53749bfa7354881874887ccc0e01554cd42f
Molochnyy <153619807+Molochnyy@users.noreply.github.com>
2024-05-21 09:36:05 +0300

    ДЗ - 3

--- js/task-2.js
+++ js/task-2.js
@@ -1,34 +1,16 @@
 'use strict';
 
 
-function formatMessage(message, maxLength) {
-    if (message.length <= maxLength) {
-        return message;
-    }
-    else {
-        return message.slice(0, maxLength) + "...";
-    }
-}
-let b = (formatMessage("Curabitur ligula sapien", 16));
-console.log(b);
-b = (formatMessage("Curabitur ligula sapien", 23));
-console.log(b);
-b = (formatMessage("Vestibulum facilisis purus nec", 20));
-console.log(b);
-b = (formatMessage("Vestibulum facilisis purus nec", 30));
-console.log(b);
-b = (formatMessage("Nunc sed turpis a felis in nunc fringilla", 15));
-console.log(b);
-b = (formatMessage("Nunc sed turpis a felis in nunc fringilla", 41));
-console.log(b);
+function makeArray(firstArray, secondArray, maxLength) {
+    const allArray = firstArray.concat(secondArray);
+    const allArrayLength = allArray.length;
+    return allArray.slice(0, maxLength);
 
-
-function formatMessage1(message1, maxLength1) {
-    return message1.length <= maxLength1 ? message1 : message1.slice(0, maxLength1) + "...";
 }
-console.log(formatMessage1("Curabitur ligula sapien", 16));
-console.log(formatMessage1("Curabitur ligula sapien", 23));
-console.log(formatMessage1("Vestibulum facilisis purus nec", 20));
-console.log(formatMessage1("Vestibulum facilisis purus nec", 30));
-console.log(formatMessage1("Nunc sed turpis a felis in nunc fringilla", 15));
-console.log(formatMessage1("Nunc sed turpis a felis in nunc fringilla", 41));
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3));
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4));
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3));
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2));
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4));
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0));
--- js/task-3.js
+++ js/task-3.js
@@ -1,90 +1,19 @@
 'use strict';
 
-
-
-// type_1
-
-function checkForSpam(message) {
-    const a = message.toLowerCase();
-    const b = a.includes("spam") || a.includes("sale");
-    if (b) {
-        console.log(true);
+function filterArray(numbers, value) {
+    let newArray = [];
+    for (const number of numbers) {
+        if (number > value) {
+            newArray.push(number);
+        }
     }
-    else {
-        console.log(false);
-    }
-    return b;
+    return newArray;
 }
 
-let d = (checkForSpam("Latest technology news"));
-console.log(d);
-d = (checkForSpam("JavaScript weekly newsletter"));
-console.log(d);
-d = (checkForSpam("Get best sale offers now!"));
-console.log(d);
-d = (checkForSpam("Amazing SalE, only tonight!"));
-console.log(d);
-d = (checkForSpam("Trust me, this is not a spam message"));
-console.log(d);
-d = (checkForSpam("Get rid of sPaM emails. Our book in on sale!"));
-console.log(d);
-d = (checkForSpam("[SPAM] How to earn fast money?"));
-console.log(d);
-
-// type_2
-
-function checkForSpam1(message1) {
-    const a1 = message1.toLowerCase();
-    const b1 = a1.includes("spam");
-    const c1 = a1.includes("sale");
-    if (b1) {
-        console.log(true);
-    }
-    else if (c1) {
-        console.log(true);
-    }
-    else {
-        console.log(false);
-    }
-    return;
-}
-
-let d1 = (checkForSpam1("Latest technology news"));
-console.log(d1);
-d1 = (checkForSpam1("JavaScript weekly newsletter"));
-console.log(d1);
-d1 = (checkForSpam1("Get best sale offers now!"));
-console.log(d1);
-d1 = (checkForSpam1("Amazing SalE, only tonight!"));
-console.log(d1);
-d1 = (checkForSpam1("Trust me, this is not a spam message"));
-console.log(d1);
-d1 = (checkForSpam1("Get rid of sPaM emails. Our book in on sale!"));
-console.log(d1);
-d1 = (checkForSpam1("[SPAM] How to earn fast money?"));
-console.log(d1);
 
 
-// type_3
-
-function checkForSpam2(message2) {
-    const a2 = message2.toLowerCase();
-    const b2 = a2.trim();
-    return b2.includes("spam") || b2.includes("sale") ? true : false;
-
-}
-
-let d2 = (checkForSpam2("Latest technology news"));
-console.log(d2);
-d2 = (checkForSpam2("JavaScript weekly newsletter"));
-console.log(d2);
-d2 = (checkForSpam2("Get best sale offers now!"));
-console.log(d2);
-d2 = (checkForSpam2("Amazing SalE, only tonight!"));
-console.log(d2);
-d2 = (checkForSpam2("Trust me, this is not a spam message"));
-console.log(d2);
-d2 = (checkForSpam2("Get rid of sPaM emails. Our book in on sale!"));
-console.log(d2);
-d2 = (checkForSpam2("[SPAM] How to earn fast money?"));
-console.log(d2);
+filterArray([1, 2, 3, 4, 5], 3);
+filterArray([1, 2, 3, 4, 5], 4);
+filterArray([1, 2, 3, 4, 5], 5);
+filterArray([12, 24, 8, 41, 76], 38);
+filterArray([12, 24, 8, 41, 76], 20);
