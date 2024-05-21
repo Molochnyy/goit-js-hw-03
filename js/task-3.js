e43fd898d9b0fec98c108174720da7ae4ac88b5a
Molochnyy <153619807+Molochnyy@users.noreply.github.com>
2024-05-21 09:42:11 +0300

    ДЗ - 3.3

--- js/task-3.js
+++ js/task-3.js
@@ -12,8 +12,8 @@ function filterArray(numbers, value) {
 
 
 
-filterArray([1, 2, 3, 4, 5], 3);
-filterArray([1, 2, 3, 4, 5], 4);
-filterArray([1, 2, 3, 4, 5], 5);
-filterArray([12, 24, 8, 41, 76], 38);
-filterArray([12, 24, 8, 41, 76], 20);
+console.log(filterArray([1, 2, 3, 4, 5], 3));
+console.log(filterArray([1, 2, 3, 4, 5], 4));
+console.log(filterArray([1, 2, 3, 4, 5], 5));
+console.log(filterArray([12, 24, 8, 41, 76], 38));
+console.log(filterArray([12, 24, 8, 41, 76], 20));
